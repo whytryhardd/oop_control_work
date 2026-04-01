6a09babbeba06cc6fb5c959c579615c18e00b82c
whytry <calach1875@gmail.com>
2026-04-01 09:57:28 +0200

    123

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    String name;
+    int age;
+
+    void sayHello() {
+        System.out.println("Hello, my name is " + name);
+    }
+
+    int getAge() {
+        return age;
+    }
+}
